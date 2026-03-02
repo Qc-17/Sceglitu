16fdac9448675f4146596915533c294810abef17
Qc-17 <qc_17_@virgilio.it>
2026-03-02 08:06:40 +0100

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-let stories = [];
-let currentLang = localStorage.getItem('sceglitu_lang') || 'it';
-
-document.addEventListener('DOMContentLoaded', () => {
-  loadStories();
-  setLanguage(currentLang);
-  renderHome();
-  updateAllTranslations();
-});
